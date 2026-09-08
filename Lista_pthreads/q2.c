7f201ced44bab69b6bf8230e463da2c0fadae510
Davi <davipinheiro1266@gmail.com>
2026-09-07 23:21:15 -0300

    ADD : Q2 init functions, thread_n struct, mutex_vector for each L line

--- Lista_pthreads/q2.c
+++ Lista_pthreads/q2.c
@@ -1,18 +1,48 @@
+/*
+Para resolvermos a questão, iremos criar uma thread específica para cada arquivo que será lido.
+Além disso, teremos funções específicas de leitura e escrita para fazer em cima de cada arquivo. 
+precisamos ter mutex para as mesmas linhas não serem acessadas ao mesmo tempo ( NUM_CONSU ).
+*/
+
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-//2. Você deve implementar uma tabela de chamada de pacientes em um consultório médico. 
-//A tabela deve ser apresentada no início e apenas as linhas necessárias devem ser atualizadas (pesquise sobre sequências de escape ANSI).
-//Cada linha é independente, ou seja, linhas diferentes podem ser atualizadas ao mesmo tempo.
-//Além disso, deve haver exclusão mútua por linha, logo, uma thread só pode modificar uma linha seela estiver livre. 
-//Uma linha deve ficar alguns segundos bloqueada antes de ser modificada novamente para que a  mudança seja visível. 
-//As informações sobre novos pacientes devem ser lidas de arquivos de texto externos (você deve criá-los). 
-//Neles, devem ser informados os nomes dos pacientes e o número do consultório. Haverá N arquivos com L linhas cada
+#define NUM_FILES 3 // quantidade de threads leitoras 
+#define NUM_CONSU 6 // de 1 a 7 -> Permanece nessa quantidade, mas pode ser alterado
 
+//declarando e inicializando o vetor de mutexes de forma global para que possamos manipular diretamete em cada struct de thread
+pthread_mutex_t mutex_consultorio[NUM_CONSU];
+
+void init_mutexes() {
+    for ( int idx=0 ; idx < NUM_CONSU ; idx++)  pthread_mutex_init(&mutex_consultorio[idx], NULL); 
+}
+
+void init_consultorio() {
+
+    FILE * f0 = fopen("pacientes_q2/pacientes_iniciais.txt", "w");
+
+    if ( f0 == NULL) {
+        printf("Erro ao criar Arquivo de pacientes\n");
+        exit(1);
+    }
+
+    for (int i=0 ; i < NUM_CONSU; i++) {
+        fprintf(f0,"LIVRE consultório %i\n",(i+1));
+    }
+
+    fclose(f0);
+}
+
+typedef struct thread_file {
+    int id; // id da thread
+    char *file; // declarando ponteiro pelo qual a thread irá ler o seu N arquivo
+} thread_n;
 
 int main() {
-    
+    init_mutexes();
+    init_consultorio();
+
     return 0;
 }
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-Teste
+Q1 - > funcionando. Ela funciona com 3 funções, fora a main: escalonador, alimentador e a de soma
+Q2 - > Em andamento. Abordagem explicada no arquivo da questão
