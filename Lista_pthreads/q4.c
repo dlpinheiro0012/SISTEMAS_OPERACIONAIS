5e9ac25fd8fa4eec5f14fc12b858cae5286b6a6f
Davi <davi.dlp@ufpe.br>
2026-09-19 17:27:25 -0300

    começando q4, a função jacobi talvez mude pq ai eu so pensei no somatorio macro

--- Lista_pthreads/q4.c
+++ Lista_pthreads/q4.c
@@ -3,8 +3,35 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main() {
+#define NUM_INCOGNITAS 10
+#define NUM_THREADS 3
 
+int vet_X[NUM_INCOGNITAS];
+int vet_coef[NUM_INCOGNITAS][NUM_INCOGNITAS];
+
+
+double somatorio_jacobi(int ** vet_coeficientes, int * vet_incognitas ) {
+    double soma = 0;
+    for ( int i = 0; i < NUM_INCOGNITAS; i++)  {
+        for (int k = 0; k < NUM_INCOGNITAS; k ++) {
+            if (i != k) {
+                soma += vet_coeficientes[i][k] * vet_incognitas[k]; // soma com pesos para os coeficietes e as incógnitas com valores da interação anterior
+            }
+        }
+    }
+
+    return soma;
+}
+
+void init_vet_incognitas() { // começamos cada incónita X como 1
+    for( int i = 0 ; i < NUM_INCOGNITAS; i++) {
+        vet_X[i] = 1;
+    }    
+}
+
+
+int main() {
+    init_vet_incognitas();
     
     return 0;
 }
