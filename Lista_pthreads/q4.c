6886c69c23d958d48741860581a963e68b6dce7b
dlp <dlp@ug5c50.windows.cin.ufpe.br>
2026-09-21 16:10:08 -0300

    funcao jacobi funcionando

--- Lista_pthreads/q4.c
+++ Lista_pthreads/q4.c
@@ -6,22 +6,30 @@
 #define NUM_INCOGNITAS 10
 #define NUM_THREADS 4
 
-int vet_X[NUM_INCOGNITAS] = {1};
+double vet_X[NUM_INCOGNITAS] = {1.0};
 int vet_coef[NUM_INCOGNITAS][NUM_INCOGNITAS]; 
-int vet_result[NUM_INCOGNITAS] = {0};
+double vet_result[NUM_INCOGNITAS] = {1.0};
 
 
-double somatorio_jacobi(int ** vet_coeficientes, int * vet_x_past ) {
-    double soma = 0;
-    for ( int i = 0; i < NUM_INCOGNITAS; i++)  {
-        for (int k = 0; k < NUM_INCOGNITAS; k ++) {
-            if (i != k) {
-                soma += vet_coeficientes[i][k] * vet_x_past[k]; // soma com pesos para os coeficietes e as incógnitas com valores da interação anterior
+double * somatorio_jacobi(int vet_coeficientes[NUM_INCOGNITAS][NUM_INCOGNITAS], double * vet_x_past , double* vet_x_new) {
+
+    int qtd_variaveis = sizeof(vet_x_new)/sizeof(double);
+            
+        for ( int i = 0; i < qtd_variaveis; i++)  {
+            double soma = 0;
+            for (int k = 0; k < NUM_INCOGNITAS; k ++) {
+                if (i != k) {
+                    soma += vet_coeficientes[i][k] * vet_x_past[k]; // soma com pesos para os coeficietes e as incógnitas com valores da interação anterior
+                }
             }
+            vet_x_new[i] = (vet_result[i] - soma)/vet_coeficientes[i][i];
         }
-    }
 
-    return soma;
+    return vet_x_new;
+}
+
+void * executar_thread(void* funcao) {
+
 }
 
 void init_vet_coef() {
